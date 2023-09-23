10360155807b4e6717226fd1e7fb3ca8e4b3e620
muffindud <ccatlabuga2002@gmail.com>
2023-09-23 18:22:58 +0300

    finish lab3 script

--- lab3/main.py
+++ lab3/main.py
@@ -8,15 +8,41 @@ def main():
         print()
 
         if choice == "E":
-            ...
+            message = input("Enter the message: ")
+            keyword = input("Enter the keyword: ")
+            print()
+
+            try:
+                result = encrypt(message, keyword)
+            except ValueError as error:
+                print(error, end="\n\n")
+                continue
+
+            print("Message: {}".format(result["message"]))
+            print("Ciphertext: {}".format(result["ciphertext"]))
+            print("Keyword: {}".format(result["keyword"]))
+            print("Encryption text: {}".format(result["encryption_text"]), end="\n\n")
         elif choice == "D":
-            ...
+            ciphertext = input("Enter the ciphertext: ")
+            keyword = input("Enter the keyword: ")
+            print()
+
+            try:
+                result = decrypt(ciphertext, keyword)
+            except ValueError as error:
+                print(error, end="\n\n")
+                continue
+
+            print("Ciphertext: {}".format(result["ciphertext"]))
+            print("Message: {}".format(result["message"]))
+            print("Keyword: {}".format(result["keyword"]))
+            print("Encryption text: {}".format(result["encryption_text"]), end="\n\n")
         elif choice == "Q":
             break
         else:
             print("Invalid choice.", end="\n\n")
+            continue
 
 
 if __name__ == '__main__':
-    # main()
-    print(encrypt("tsaamstuulhum", "sadsscatt"))
+    main()
--- lab3/playfair.py
+++ lab3/playfair.py
@@ -73,10 +73,46 @@ def decrypt(ciphertext: str, keyword: str) -> dict:
     if any(letter not in upper_alphabet for letter in ciphertext):
         raise ValueError("The ciphertext must contain only letters and spaces.")
 
-    if ciphertext % 2 == 1:
+    if len(ciphertext) % 2 == 1:
         raise ValueError("The ciphertext must have an even number of letters.")
 
     if any(letter not in upper_alphabet for letter in keyword):
         raise ValueError("The keyword must contain only letters and spaces.")
 
-    
+    # Create the encryption alphabet
+    for letter in keyword:
+        if letter not in encryption_text:
+            encryption_text += letter
+
+    for letter in upper_alphabet:
+        if letter not in encryption_text and letter != "Â":
+            encryption_text += letter
+
+    # Create the encryption matrix
+    for i in range(6):
+        encryption_matrix.append(encryption_text[i * 5:i * 5 + 5])
+
+    # Decrypt the message
+    message = ""
+    for i in range(0, len(ciphertext), 2):
+        pair = ciphertext[i:i + 2]
+
+        # Find the position of the letters in the encryption matrix
+        for row in range(6):
+            for column in range(5):
+                if encryption_matrix[row][column] == pair[0]:
+                    first_letter = (row, column)
+                if encryption_matrix[row][column] == pair[1]:
+                    second_letter = (row, column)
+
+        if first_letter[0] == second_letter[0]:
+            message += encryption_matrix[first_letter[0]][(first_letter[1] - 1) % 5]
+            message += encryption_matrix[second_letter[0]][(second_letter[1] - 1) % 5]
+        elif first_letter[1] == second_letter[1]:
+            message += encryption_matrix[(first_letter[0] - 1) % 6][first_letter[1]]
+            message += encryption_matrix[(second_letter[0] - 1) % 6][second_letter[1]]
+        else:
+            message += encryption_matrix[first_letter[0]][second_letter[1]]
+            message += encryption_matrix[second_letter[0]][first_letter[1]]
+
+    return {"message": message, "ciphertext": ciphertext, "keyword": keyword, "encryption_text": encryption_text}
